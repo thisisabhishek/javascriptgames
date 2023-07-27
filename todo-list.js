ac00248d8c8496a19037045516fea896ccc2846f
Abhishek Ahlawat <gogi7007@gmail.com>
2023-07-27 15:49:57 +0530

    Update Todo app
    Added a simple comment

--- todo-list.js
+++ todo-list.js
@@ -5,6 +5,7 @@
   done:Some task = Marks 'Some Task' as completed
   show = Show the Todo List tasks with Status
   clear = Clear the Tasks
+  exit = To exit the application
 */
 let action = prompt("Welcome to TODO App!\n\nCOMMANDS:\nadd:Some Task = Adds 'Some Task' to the Todo list\ndone:Some task = Marks 'Some Task' as completed\nshow = Show the Todo List tasks with Status\nclear = Clear the Tasks\n")
 console.log(action)
