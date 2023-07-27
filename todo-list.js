27daa9d82b8bbfcbac658190e990cdb6361d6bd0
Studytonight <140691074+studytonightfun@users.noreply.github.com>
2023-07-27 12:34:30 +0530

    Create todo-list.js
    Adding a new simple JS project file

---
+++ todo-list.js
@@ -0,0 +1,8 @@
+// This will have a basic TODO Application
+/*
+  User Input and Action:
+  add:Some Task = Adds 'Some Task' to the Todo list
+  done:Some task = Marks 'Some Task' as completed
+  show = Show the Todo List tasks with Status
+  clear = Clear the Tasks
+*/
