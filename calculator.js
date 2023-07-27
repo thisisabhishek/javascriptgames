8261208f90a2089b51e57e98bf965632772b3d44
Studytonight <140691074+studytonightfun@users.noreply.github.com>
2023-07-27 12:37:27 +0530

    Create calculator.js
    Basic prompt style calculator application

---
+++ calculator.js
@@ -0,0 +1,6 @@
+/*
+  Simple Calculator using Prompt box
+  - user can input expressions like 2 + 2 and get the result
+  - We should show the result in Prompt so that it can be used again to perform more calculation
+  - Can add more features if required.
+*/
