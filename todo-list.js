04acdc93c98fa5d5d42198646445d79dba79e439
Abhishek Ahlawat <gogi7007@gmail.com>
2023-07-27 12:43:18 +0530

    Update Todo App
    Adding the first prompt with commands to use Todo app

--- todo-list.js
+++ todo-list.js
@@ -6,3 +6,5 @@
   show = Show the Todo List tasks with Status
   clear = Clear the Tasks
 */
+let action = prompt("Welcome to TODO App!\n\nCOMMANDS:\nadd:Some Task = Adds 'Some Task' to the Todo list\ndone:Some task = Marks 'Some Task' as completed\nshow = Show the Todo List tasks with Status\nclear = Clear the Tasks\n")
+console.log(action)
